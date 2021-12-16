1a4fcae153010d8ca61a9abd08a7d3b38513ad08
Dinuka Thenuwara <dthenuw@apac.corpdir.net>
2021-12-16 13:18:52 +0800

    clean up - Delete MVC related folders from API project

--- JobAdder.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace JobAdder.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- JobAdder.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace JobAdder.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
