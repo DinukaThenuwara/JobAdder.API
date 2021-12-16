d40dda190f4f995af932d8c890aa0f9392419c9f
Dinuka Thenuwara <dthenuw@apac.corpdir.net>
2021-12-16 20:50:21 +0800

    Enable CORS

--- JobAdder.API/App_Start/WebApiConfig.cs
+++ JobAdder.API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace JobAdder.API
 {
@@ -9,7 +10,10 @@ namespace JobAdder.API
     {
         public static void Register(HttpConfiguration config)
         {
+
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- JobAdder.API/Controllers/JobController.cs
+++ JobAdder.API/Controllers/JobController.cs
@@ -7,9 +7,11 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace JobAdder.API.Controllers
 {
+    //[EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     [RoutePrefix("api/job")]
     public class JobController : ApiController
     {
